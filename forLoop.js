d633348ccd75d0bbd17f4accc2a3602233771f67
thisisshwetadubey <dubeyshweta7049@gmail.com>
2023-07-16 01:46:10 +0530

    loop

---
+++ forLoop.js
@@ -0,0 +1,9 @@
+const data = [1, 2];
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+   for(let k = 0; k<  data.length; k++){
+        console.log("i:::j::::k::::", i,j,k);
+
+   }
+  }
+}
